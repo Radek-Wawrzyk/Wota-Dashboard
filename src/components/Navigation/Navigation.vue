4f95b22cc44ebf7a875a2a18c55c91ffa34fdeee
Radek <radek511@op.pl>
2019-01-20 18:30:18 +0000

    Add - Navigation component with styles

---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navigation">
+    <div class="navigation-column">
+      <figure class="navigation-logo">
+        <img src="../../assets/logoC.png" alt="Wota" />
+      </figure>
+      <button class="navigation-button" :class="{'active': menuStatus}" :aria-expanded="menuStatus ? 'true' : 'false'" @click="toggleMenu" aria-label="Open menu">
+        <span class="navigation-button-bar"></span>
+        <span class="navigation-button-bar"></span>
+        <span class="navigation-button-bar"></span>
+      </button>
+    </div>
+    <div class="navigation-column">
+      <ul class="navigation-settings">
+        <li class="navigation-settings-item">
+          <span class="navigation-user">Admin</span>
+        </li>
+        <li class="navigation-settings-item">
+          <el-button type="danger" round>
+            Logout
+          </el-button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Navigation',
+  props: [
+    'menuStatus'
+  ],
+  methods: {
+    toggleMenu() {
+      this.$emit('toggleMenu', this.menuStatus);
+    }
+  }
+};
+
+</script>
+
+<style scoped src="./Navigation.scss" lang="scss" />
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>Hello there</h1>
+  <div class="home">
+    <Navigation v-on:toggleMenu="toggleMenu($event)" :menuStatus="menuStatus"></Navigation>
+    <!-- <Menu></Menu> -->
+    <main class="main">
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
 <script>
+import Navigation from '../components/Navigation/Navigation.vue';
+//import Menu from '../components/Menu/Menu.vue';
+
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  components: { 
+    Navigation,
+   // Menu
+  },
+  data: () => ({
+    menuStatus: false
+  }),
+  methods: {
+    toggleMenu(menuStatus) {
+      this.menuStatus =! menuStatus;
+    }
+  }
 };
 </script>
