cc03aa8c639e09689d3face45268c8b408036f46
Radek <radek511@op.pl>
2019-02-03 16:40:06 +0000

    Fix - Vuex bug

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,12 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 //Modules
-import instructors from './modules/instructors.js';
 import auth from './modules/auth.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    instructors,
     auth
   }
 });
